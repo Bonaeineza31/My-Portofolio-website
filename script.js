dbaf0c8371065dafac309f024808afb22e9a69fe
Bonaeineza31 <b.ineza@alustudent.com>
2025-01-10 23:39:03 +0200

    updated

---
+++ script.js
@@ -0,0 +1,14 @@
+/* const chatbotContainer = document.querySelector('.chatbot');
+ chatbotContainer.addEventListener('click', () => {
+    const userInput = prompt('How can I assist you today?');
+    if (userInput) {
+        setTimeout(() => {
+            alert('Fetching details... Please wait.');
+            setTimeout(() => {
+                alert('Here are the details. Leave your email for further contact.');
+                const email = prompt('Enter your email:');
+                if (email) alert('Thank you! We will reach out soon.');
+            }, 2000);
+        }, 1000);
+    }
+});*/
